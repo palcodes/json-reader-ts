bdcc430432918ef8d5fdf3df41b90244220f4fe4
Aayush Pal <ap.aayushpal@gmail.com>
2020-05-21 03:52:57 +0530

    comments

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,12 @@ import * as data from './mcqs.json';
 
 const app = express();
 
+// API -> show JSON data in the browser
 app.get('/json', (req, res) => {
     res.send(data.default);
 });
 
+// Server runner
 app.listen(3000, () => {
     console.log('Server Running..');
 });
